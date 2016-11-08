f47684b0eadef77452f4d98f519c4e955339b8cf
nullice <ui@nullice.com>
2016-11-08 08:55:17 +0800

    0

---
+++ demo/demo.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bgllj on 2016/11/7.
+ */
+
+
+
+
+
+console.log("133323")
---
+++ webpack_BUILD.config.js
@@ -0,0 +1,37 @@
+/**
+ * Created by bgllj on 2016/5/25.
+ */
+const webpack = require('webpack');
+const path = require('path');
+
+
+module.exports = {
+    entry: {
+        main:['babel-polyfill','./demo/demo.js'],
+    },
+    output: {
+        path: './demo',
+        filename: 'demo-main.js'
+    },
+    target: 'web',
+
+    module:{
+        loaders:[
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+
+            ]},
+    plugins: [
+    ],
+    devtool: 'eval',
+
+    vue: {
+        loaders: {
+        }
+    },
+    babel: {
+        // enable stage 0 babel transforms.
+        presets: ['es2015', 'stage-0'],
+        plugins: ['transform-runtime']
+    }
+};
+
