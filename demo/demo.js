5bda7d1dfa2ed6fc4ad35b047aa35f3a808b14ba
nullice <ui@nullice.com>
2016-11-08 09:25:55 +0800

    demo

--- demo/demo.js
+++ demo/demo.js
@@ -2,8 +2,21 @@
  * Created by bgllj on 2016/11/7.
  */
 
+import IchiColor from "./../src/ichi-color.js"
 
 
+var mainVue = new Vue({
+    el: 'body',
+    data: {},
+    components: {
+        // include the required component
+        // in the options
+        "attr-panel": AttrPanel,
+        "layers-panel": LayerSelectors,
+        "var-panel": VarPanel,
+        "expression-panel": ExpressionPanel
+    }
+})
 
-
+window.mainVue = mainVue;
 console.log("133323")
